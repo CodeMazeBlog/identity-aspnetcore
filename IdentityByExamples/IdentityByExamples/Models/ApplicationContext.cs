dba31e96fa751b9b69061c96451afb930b80157b
Marinko <marinkospasojevic@gmail.com>
2020-01-12 09:55:11 +0100

    Added starting project source code.

---
+++ IdentityByExamples/IdentityByExamples/Models/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using IdentityByExamples.Models.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityByExamples.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # identity-aspnetcore
+##  https://code-maze.com/identity-asp-net-core-project/ 
 This repo contains the source code for "Identity in ASP.NET Core series" on Code Maze
